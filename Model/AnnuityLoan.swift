140d006dfe7325efdb4e5e66f5914e7714906e16
LouisGrdn <130607922+LouisGrdn@users.noreply.github.com>
2024-03-27 23:26:58 +0100

    Adding new calculation

--- Model/AnnuityLoan.swift
+++ Model/AnnuityLoan.swift
@@ -9,36 +9,20 @@ import Foundation
 
 class AnnuityLoan: Loan {
     
-    func annuity() -> Float {
-        return self.amount * (self.rate / (1 - pow(1 + self.rate, Float(self.time * -1))))
-    }
-    
-    func getStartingCapital(periode: Int) -> Float {
-        var result = self.amount
-        if periode > 0 {
-            result = getEndingCapital(periode: periode - 1)
-        }
-        return result
-    }
-    
-    func getInterest(periode: Int) -> Float {
-        return getStartingCapital(periode: periode) * self.rate
-    }
-    
-    func getAmortissement(periode: Int) -> Float {
-        return annuity() - getInterest(periode: periode)
-    }
-    
-    func getEndingCapital(periode: Int) -> Float {
-        let calcul = getStartingCapital(periode: periode) - getAmortissement(periode: periode)
-//        return calcul
-        switch(true) {
-        case calcul < 0:
-            return calcul * -1
-        case calcul < 1:
-            return 0
-        default:
-            return calcul
+    override init(amount: Float, rate: Float, time: Int, periode: Loan.Periode) {
+        super.init(amount: amount, rate: rate, time: time, periode: periode)
+        self.annuity = self.amount * (self.rate / (1 - pow(1 + self.rate, Float(self.time * -1))))
+        for i in 0..<self.time {
+            i == 0 ? start.append(self.amount) : start.append(end[i - 1])
+            interests.append(start[i] * self.rate)
+            amortis.append(self.annuity - interests[i])
+            end.append(start[i] - amortis[i])
         }
     }
+    
+    var start: Array<Float> = []
+    var end: Array<Float> = []
+    var annuity: Float = 0
+    var interests: Array<Float> = []
+    var amortis: Array<Float> = []
 }
